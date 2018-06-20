a55d651c41d05378807573d5770d58ad4fa23d79
Matteo Maronati <matteo.maronati@flixbus.com>
2018-06-20 14:40:00 +0200

    Mroe meaningful logs for header validation

--- validate/validate-headers.js
+++ validate/validate-headers.js
@@ -2,10 +2,10 @@ module.exports = async (expectedHeaders, headers) => {
   for (var header in expectedHeaders) {
     let validHeader = expectedHeaders[header] === headers[header]
     if (validHeader) {
-      console.log(`Valid header: ${header}: ${headers[header]}`)
+      console.log(`Passed header validation: '${header}': '${headers[header]}'`)
     }
     if (!validHeader) {
-      console.error(`Invalid header code: ${headers[header]}, expected ${expectedHeaders[header]}`)
+      console.error(`Did not pass header validation: '${header}' : '${expectedHeaders[header]}'`)
     }
   }
 }
