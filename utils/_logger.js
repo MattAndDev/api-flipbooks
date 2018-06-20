08a27cf7b47d51a43587cfd890c1ef8a3d3df55e
Matteo Maronati <matteo.maronati@flixbus.com>
2018-06-20 14:52:24 +0200

    Minimal logger utility to avoid acccessing console directly, useful for future

---
+++ utils/_logger.js
@@ -0,0 +1,18 @@
+/**
+*  logger
+*
+*  @param {String} type - type of log
+*  @param {Sring} message - message to log
+*  @return {Function} console expression
+*/
+const logger = (type, message) => {
+  if (type === 'error') {
+    return console.error('\x1b[31m%s\x1b[0m', message)
+  }
+  if (type === 'success') {
+    return console.log('\x1b[32m%s\x1b[0m', message)
+  }
+  throw `Utils.logger does not support type: ${type}`
+}
+
+module.exports = logger
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   checkType: require('./_check-type'),
+  logger: require('./_logger'),
   mapArgs: require('./_map-args'),
   matchAllFilesRecursively: require('./_match-files-recursively'),
   request: require('./_request')
